7aee0ee6138a9645e309ec9079a590e3c9d0a2b4
Justin O'Dwyer <justin.odwyer@datadoghq.com>
2025-10-12 03:24:23 -0400

    Added debugger support.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,3 +17,21 @@ vim.keymap.set("n", "<leader>yp", function()
   vim.fn.setreg("+", vim.fn.expand("%:p"))
   print("Copied file path: " .. vim.fn.expand("%:p"))
 end, { desc = "Copy full file path" })
+
+-- DAP (Debug Adapter Protocol) keybindings
+map("n", "<leader>db", "<cmd>DapToggleBreakpoint<cr>", { desc = "Toggle breakpoint" })
+map("n", "<leader>dc", "<cmd>DapContinue<cr>", { desc = "Start/Continue debugging" })
+map("n", "<leader>di", "<cmd>DapStepInto<cr>", { desc = "Step into" })
+map("n", "<leader>do", "<cmd>DapStepOver<cr>", { desc = "Step over" })
+map("n", "<leader>dO", "<cmd>DapStepOut<cr>", { desc = "Step out" })
+map("n", "<leader>dt", "<cmd>DapTerminate<cr>", { desc = "Terminate debugging" })
+map("n", "<leader>dr", "<cmd>lua require('dap').repl.open()<cr>", { desc = "Open REPL" })
+map("n", "<leader>dl", "<cmd>lua require('dap').run_last()<cr>", { desc = "Run last debug config" })
+map("n", "<leader>du", "<cmd>lua require('dapui').toggle()<cr>", { desc = "Toggle DAP UI" })
+
+-- Arrow key mappings for debugging (intuitive directional stepping)
+map("n", "<Down>", "<cmd>DapStepOver<cr>", { desc = "DAP: Step over" })
+map("n", "<Right>", "<cmd>DapStepInto<cr>", { desc = "DAP: Step into" })
+map("n", "<Left>", "<cmd>DapStepOut<cr>", { desc = "DAP: Step out" })
+map("n", "<Up>", "<cmd>lua require('dap').restart_frame()<cr>", { desc = "DAP: Restart frame" })
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,5 +56,58 @@ return {
                 debounce_delay = 250,
             })
         end,
-    }
+    },
+
+    -- DAP (Debug Adapter Protocol).
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+        },
+        cmd = {
+            "DapToggleBreakpoint",
+            "DapContinue",
+            "DapStepInto",
+            "DapStepOver",
+            "DapStepOut",
+            "DapTerminate",
+        },
+        keys = {
+            { "<leader>db", "<cmd>DapToggleBreakpoint<cr>", desc = "Toggle breakpoint" },
+            { "<leader>dc", "<cmd>DapContinue<cr>", desc = "Start/Continue debugging" },
+            { "<leader>di", "<cmd>DapStepInto<cr>", desc = "Step into" },
+            { "<leader>do", "<cmd>DapStepOver<cr>", desc = "Step over" },
+            { "<leader>dO", "<cmd>DapStepOut<cr>", desc = "Step out" },
+            { "<leader>dt", "<cmd>DapTerminate<cr>", desc = "Terminate debugging" },
+            { "<leader>dr", "<cmd>lua require('dap').repl.open()<cr>", desc = "Open REPL" },
+            { "<leader>dl", "<cmd>lua require('dap').run_last()<cr>", desc = "Run last debug config" },
+            { "<leader>du", "<cmd>lua require('dapui').toggle()<cr>", desc = "Toggle DAP UI" },
+        },
+        config = function()
+            require "configs.dap"
+        end,
+    },
+
+
+    -- DAP UI.
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+            dapui.setup()
+
+            -- Automatically open/close DAP UI
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end,
+    },
 }
