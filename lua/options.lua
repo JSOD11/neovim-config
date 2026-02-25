89c44612e04f2c90c0dd100f2f0beee773ca9fe2
Justin O'Dwyer <justinsodwyer@gmail.com>
2026-02-25 09:06:32 -0500

    Remap :term to :t.

--- lua/options.lua
+++ lua/options.lua
@@ -11,3 +11,5 @@ o.shiftwidth = 4
 o.softtabstop = 4
 o.expandtab = true
 
+-- Expand :t to :term only when the command is exactly "t".
+vim.cmd [[cnoreabbrev <expr> t ((getcmdtype() == ':' && getcmdline() ==# 't') ? 'term' : 't')]]
