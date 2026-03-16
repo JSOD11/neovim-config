60139c8ca2a7d6b3c620b7517743a06fe1af0e69
JSOD11 <justinodwyer@college.harvard.edu>
2026-03-16 23:41:17 +0000

    suggestion handling

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,18 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 vim.diagnostic.config {
-  virtual_text = {
-    prefix = "",
-    spacing = 2,
-    source = "if_many",
-    format = function(diagnostic)
-      local message = diagnostic.message:gsub("%s+", " ")
-      if #message > 100 then
-        return message:sub(1, 97) .. "..."
-      end
-      return message
-    end,
-  },
+  virtual_text = false,
   float = {
     border = "single",
     source = "if_many",
